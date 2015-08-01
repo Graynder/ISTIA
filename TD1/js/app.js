572d4fcdd72e3b6c3f3488d566ac01f0483f5f5a
Graynder <florianbappel@gmail.com>
2015-08-01 15:19:37 +0200

    Refactoring to MVC Model & add functiion load and deleteTodo

--- TD1/js/app.js
+++ TD1/js/app.js
@@ -1,22 +1,66 @@
+/*Model*/
+var todos = [
+    {
+        todo:'todo 1',
+        completed: false
+    },
+    {
+        todo:'todo 2',
+        completed:true
+    }
+];
 
-    function addTask(e){
+/******************************************************************************/
+
+/*Controller*/
+
+//chargement des datas
+window.onload = function() {
+    generateView();
+};
+
+//Ajout d'une todo
+function addTodo(e){
 
     if (e.charCode === 13) {
-        var newTodo = document.getElementById('new-todo').value.toString();
-        generateView(newTodo);
+        var newTodo = document.getElementById('new-todo').value;
+        todos.unshift({todo:newTodo,completed:false});
+        generateView();
+        document.getElementById('new-todo').value = "";
     }
 };
 
-function generateView(newTask) {
-    var todos = document.getElementById('todo-list');
-    var i = todos.childElementCount + 1;
+//Suppression de la todo
+function deleteTodo(id) {
 
-    var todo =  '<li data-id='
-                + i
-                +' class=""><div class="view"><input class="toggle" type="checkbox" ><label>'
-                + newTask
-                +'</label><button class="destroy"></button></div></li> ';
+    todos.splice(id,1);
+    generateView();
+};
 
-    todos.innerHTML = todo + todos.innerHTML;
+/******************************************************************************/
 
+/*View*/
+function generateView() {
+
+    var i = todos.length;
+    var listTodo = document.getElementById('todo-list');
+
+    var strTodos = "";
+
+    todos.forEach(function (item, index, array) {
+
+        strTodos += '<li data-id='
+                    + index
+                    + ' class='
+                    + (item.completed?'completed':'')
+                    + '><div class="view"><input class="toggle" type="checkbox"'
+                    + (item.completed?'checked':'')
+                    + '><label>'
+                    + item.todo
+                    + '</label><button class="destroy" onclick="deleteTodo('
+                    + index
+                    + ')"></button></div></li> ';
+    });
+
+    listTodo.innerHTML = strTodos;
 };
