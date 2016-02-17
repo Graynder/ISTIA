942a58f6ff7616498b72203cf563bf92b10613ea
Graynder <florianbappel@gmail.com>
2016-02-17 20:20:30 +0100

    TD 3 modif design

--- TD3/server.js
+++ TD3/server.js
@@ -1,58 +1,71 @@
 var http = require('http');
+var md5 = require('MD5');
 
-
-httpServer = http.createServer(function(req,res) {
-  res.end('Bienvenue');
+httpServer = http.createServer(function(req, res){
+	console.log('Un utilisateur a affiche la page');
 });
+
+httpServer.listen(1337);
 
 var io = require('socket.io').listen(httpServer);
+var users = {};
+var messages = [];
+var history = 2;
 
-// Nouvelle fonction permettant de supprimer un élément dans un tableau
-Array.prototype.unset = function(val){
-var index = this.indexOf(val)
-  if(index > -1){
-    this.splice(index,1)
-  }
-}
+io.sockets.on('connection', function(socket){
 
-var myUsers = new Array; //variable globale qui contiendra l'ensemble des utilisateurs
+	var me = false;
+	console.log('Nouveau utilisateur');
 
-io.sockets.on('connection',function(socket) {
-    var me = false;
+	for(var k in users){
+		socket.emit('newusr', users[k]);
+	}
+	for(var k in messages){
+		socket.emit('newmsg', messages[k]);
+	}
 
-    console.log('Nouveau utilisateur');
+	/**
+	* On a reçu un message
+	**/
+	socket.on('newmsg', function(message){
+		// Si le message est vide on ne fait rien
+		if(message == ''){
+			return false;
+		}
+		message.user = me;
+		date = new Date();
+		message.h = date.getHours();
+		message.m = date.getMinutes();
+		messages.push(message);
+		if(messages.length > history){
+			messages.shift();
+		}
+		io.sockets.emit('newmsg', message);
+	});
 
-    for(var i=0 ; i < myUsers.length ; i++ ){
-        socket.emit('newusr',myUsers[i]);  //on envoie au nouvel utilisateur l'ensemble des users déjà connectés
-    }
+	/**
+	* Je me connecte
+	**/
+	socket.on('login', function(user){
+		me = user;
+		console.log(user.mail);
+		me.id = md5(user.mail);	// On génère une clef unique par utilisateur (basé sur le mail)
+		me.mail = 'null'; 		// On ne renvoit pas l'email publiquement
+		me.avatar = 'https://gravatar.com/avatar/' + md5(user.mail) + '?s=50';
+		socket.emit('logged');
+		users[me.id] = me;
+		io.sockets.emit('newusr', me);
+	});
 
-    socket.on('newmsg', function(message){
-        message.user = me; //on va ajouter le nom de l'user au message
-        date = new Date(); //on va ajouter l'heure du message
-        message.h = date.getHours();  //heure
-        message.m = date.getMinutes(); //minutes
-        io.sockets.emit('newmsg',message); //on emet un message 'newmsg' à destination des clients
-    });
-
-    socket.on('login',function(user){
-        me = user;
-        me.id = user.mail.replace('@','-').replace('.','-');
-        socket.emit('logged');
-
-        myUsers.push(me);  //on ajoute en fin de tableau le nouvel utilisateur
-
-        io.sockets.emit('newusr',me);
-    });
-
-    //on détecte une déconnexion
-    socket.on('disconnect', function(){
-        if(!me){
-            return false;
-        }
-        myUsers.unset(me); //on supprime l'utilisateur du tableau
-        io.sockets.emit('disusr',me); //on émet le signal disusr au client pour qu'il puisse enlever l'utilisateur de la liste
-    });
+	/**
+	* Je quitte le tchat
+	**/
+	socket.on('disconnect', function(){
+		if(!me){
+			return false;
+		}
+		delete users[me.id];
+		io.sockets.emit('disusr', me);
+	})
 
 });
-
-httpServer.listen(8080);
